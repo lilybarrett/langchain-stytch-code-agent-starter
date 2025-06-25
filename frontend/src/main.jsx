67db6cee2cbcdf16c6f67c89f2d741f27adef619
Lily Barrett <lilybarrett@Lilys-MacBook-Air-2021.local>
2025-06-25 12:30:05 -0400

    WIP

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StytchB2BProvider } from '@stytch/react/b2b';
+import { StytchB2BUIClient } from '@stytch/vanilla-js/b2b';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// optional object for configuring SDK cookie behavior, currently showing defaults
+const stytchOptions = {
+  cookieOptions: {
+    opaqueTokenCookieName: "stytch_session",
+    jwtCookieName: "stytch_session_jwt",
+    path: "",
+    availableToSubdomains: false,
+    domain: "",
+  }
+}
+
+const stytch = new StytchB2BUIClient(
+  import.meta.env.STYTCH_PUBLIC_TOKEN, // or process.env.STYTCH_PUBLIC_TOKEN for non-Vite based projects
+  stytchOptions
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <StytchB2BProvider stytch={stytch}>
+      <App />
+    </StytchB2BProvider>
+  </React.StrictMode>
+);
