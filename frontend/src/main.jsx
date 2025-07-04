b67fd9ed76e69bd3385489012fe84791032ab045
Lily Barrett <lilybarrett@Lilys-MacBook-Air-2021.local>
2025-07-04 14:32:38 -0400

    Remove unnecessary object with default values

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,20 +6,8 @@ import ReactDOM from 'react-dom/client';
 import { App } from './App.jsx';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-// optional object for configuring SDK cookie behavior, currently showing defaults
-const stytchOptions = {
-  cookieOptions: {
-    opaqueTokenCookieName: 'stytch_session',
-    jwtCookieName: 'stytch_session_jwt',
-    path: '',
-    availableToSubdomains: false,
-    domain: '',
-  },
-};
-
 const stytch = new StytchB2BUIClient(
-  import.meta.env.VITE_STYTCH_PUBLIC_TOKEN, // or process.env.STYTCH_PUBLIC_TOKEN for non-Vite based projects
-  stytchOptions
+  import.meta.env.VITE_STYTCH_PUBLIC_TOKEN // or process.env.STYTCH_PUBLIC_TOKEN for non-Vite based projects
 );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
