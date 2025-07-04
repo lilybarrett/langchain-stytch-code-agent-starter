5a0eb183815fda8299478f6467635d1571e38ac3
Lily Barrett <lilybarrett@Lilys-MacBook-Air-2021.local>
2025-07-04 16:29:38 -0400

    Remove outdated todo

--- frontend/src/utils/useRecentTopics.js
+++ frontend/src/utils/useRecentTopics.js
@@ -3,7 +3,6 @@ import { useState, useCallback } from 'react';
 export const useRecentTopics = (initialTopics = []) => {
   const [recentTopics, setRecentTopics] = useState(initialTopics);
 
-  // TODO get this working again
   const addTopic = useCallback((topic) => {
     setRecentTopics((prevTopics = []) => {
       const updatedTopics = [topic, ...prevTopics].slice(0, 5); // Keep only the last 5 topics
