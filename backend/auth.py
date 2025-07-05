7e1fa4b833303366bac92738e13fd626d6b9efdc
Lily Barrett <lilybarrett@Lilys-MacBook-Air-2021.local>
2025-07-04 21:47:34 -0400

    Clearer variable naming

--- backend/auth.py
+++ backend/auth.py
@@ -30,10 +30,8 @@ can_user_create_topic_auth_check = {
 def can_user_create_topic(authorization: str = Header(...)):
     token = authorization.removeprefix("Bearer ").strip()
     try:
-        user_data = verify_session_token(
-            token, auth_check=can_user_create_topic_auth_check
-        )
-        if not user_data or not user_data.organization:
+        data = verify_session_token(token, auth_check=can_user_create_topic_auth_check)
+        if not data or not data.organization:
             logger.error("User or organization not found in session")
             raise HTTPException(status_code=401, detail="Auth error")
         return True
@@ -45,11 +43,11 @@ def can_user_create_topic(authorization: str = Header(...)):
 def get_current_user_and_organization(authorization: str = Header(...)):
     token = authorization.removeprefix("Bearer ").strip()
     try:
-        user_data = verify_session_token(token)
-        if not user_data or not user_data.organization:
+        data = verify_session_token(token)
+        if not data or not data.organization:
             logger.error("User or organization not found in session")
             raise HTTPException(status_code=401, detail="Auth error")
-        return user_data, user_data.organization
+        return data, data.organization
     except Exception as e:
         logger.error(f"Auth failed: {e}")
         raise HTTPException(status_code=401, detail="Auth error")
